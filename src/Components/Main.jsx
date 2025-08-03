1275e60c8acf3cd355d4f1040de543d987b2c541
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-08-03 13:53:27 -0600

    added border

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,8 +2,8 @@ const Main = () => {
   return (
     <div className="mt-18 flex flex-col justify-center rounded-t-xl border border-white/20 bg-blue-300/20 shadow-xl backdrop-blur-md">
       <div className="mr-10 flex h-[64px] items-center justify-end">
-        <i className="fa-brands fa-linkedin m-4 p-2 text-4xl text-white hover:bg-gradient-to-t hover:from-purple-400 hover:via-cyan-500 hover:to-blue-700"></i>
-        <i className="fa-brands fa-github p-2 text-4xl text-white hover:bg-gradient-to-t hover:from-purple-400 hover:via-cyan-500 hover:to-blue-700"></i>
+        <i className="fa-brands fa-linkedin m-4 p-2 text-4xl text-white hover:rounded-xl hover:bg-gradient-to-t hover:from-purple-400/70 hover:via-cyan-500/70 hover:to-blue-700/70"></i>
+        <i className="fa-brands fa-github p-2 text-4xl text-white hover:rounded-xl hover:bg-gradient-to-t hover:from-purple-400/70 hover:via-cyan-500/70 hover:to-blue-700/70"></i>
       </div>
 
       <div className="h-[1000px] bg-white">
